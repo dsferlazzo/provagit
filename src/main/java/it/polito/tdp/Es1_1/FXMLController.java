79b65065788bbc4256eb14810ec6cedc6706ee48
Daniele Sferlazzo <S272348@studenti.polito.it>
2022-03-07 09:15:34 +0100

    Versione iniziale

---
+++ src/main/java/it/polito/tdp/Es1_1/FXMLController.java
@@ -0,0 +1,68 @@
+package it.polito.tdp.Es1_1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+
+    @FXML
+    private Label txtMessaggio;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void handleAccedi(ActionEvent event) {
+    	String nome = txtNome.getText();
+    	String password = txtPassword.getText();
+    	if(nome.length()!=0 && password.length()!=0)
+    	{
+    		int flNum=0;
+    		int flUpper=0;
+    		int flChar=0;
+    		
+    		for(int i = 0;i<password.length();i++)
+    		{
+    			char c = password.charAt(i);
+    			
+    			
+    			if(Character.isDigit(c))
+    				flNum=1;
+    			if(Character.isUpperCase(c))
+    				flUpper=1;
+    			if(c=='!' || c=='?' || c=='@' || c=='#')
+    				flChar=1;
+    			
+    			if(flNum==1 && flUpper==1 && flChar==1 && password.length()>=7)
+    			{
+    				txtMessaggio.setText("Accesso effettuato con successo");
+    				txtNome.setText("");
+    				txtPassword.setText("");
+    			}
+    			
+    			else
+    			{
+    				txtMessaggio.setText("ERRORE! Questa non e' una password");
+    			}
+    			
+    			
+    			
+    		}
+    		
+    		
+    		
+    		
+    	}
+    	else
+    	{
+    		txtMessaggio.setText("ERRORE! Nome o password ancora da inserire");
+    	}
+
+    }
+
+}
---
+++ src/main/java/it/polito/tdp/Es1_1/Main.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.Es1_1;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
